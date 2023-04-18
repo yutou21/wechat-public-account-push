d7006965982442df073d77a9792ada2920a55d5f
yutou21 <131137116+yutou21@users.noreply.github.com>
2023-04-18 15:54:59 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'orZU665n7ZreDccXgrLX4CfsmR1A',
+      id: 'orZU66zu4_MFedLF_ObPJVXTi1Ic',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '05-17',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -36,7 +36,7 @@ const USER_CONFIG = {
           type: '生日', name: '芋头', year: '2000', date: '05-17',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2020', date: '11-17',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -53,9 +53,9 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'orZU66zu4_MFedLF_ObPJVXTi1Ic',
+      name: '十一点半睡觉',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'orZU66zu4_MFedLF_ObPJVXTi1Ic',
     }
   ],
 
