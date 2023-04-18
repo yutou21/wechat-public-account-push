e157df5a4830a747b39919331246867edf7325e1
yutou21 <131137116+yutou21@users.noreply.github.com>
2023-04-18 16:15:34 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '	orZU66zu4_MFedLF_ObPJVXTi1Ic',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'zJ-tEwXi3svn74Owx6GpIeXm5J6iZ_0uMz_HJouC8gs',
+      useTemplateId: '	cgbj-C9et_VKxUDT0Tqlie2qnszgh7C1HLBkR_gDjJA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-14',
       festivals: [
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'zJ-tEwXi3svn74Owx6GpIeXm5J6iZ_0uMz_HJouC8gs',
+  CALLBACK_TEMPLATE_ID: '	cgbj-C9et_VKxUDT0Tqlie2qnszgh7C1HLBkR_gDjJA',
 
   CALLBACK_USERS: [
     {
